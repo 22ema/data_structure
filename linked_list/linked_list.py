70e587400ea2c542dcc853ebf6807872b7b39b05
22ema <yunsung9503@gmail.com>
2023-09-21 00:19:22 +0900

    LinkedList: add function
     - add append_left func
     - add get func
     - add len func
    
     #1

---
+++ linked_list/linked_list.py
@@ -0,0 +1,38 @@
+class Node():
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+
+class LinkedList():
+    def __init__(self):
+        self.head = None
+    
+    def append_left(self, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+        else:
+            current_node = self.head
+            while(current_node.next):
+                current_node = current_node.next
+            current_node.next = node
+
+    def get(self, idx):
+        if self.len() <= idx:
+            print("out of index")
+            return None
+        current_node = self.head
+        for _ in range(idx):
+            current_node = current_node.next
+        return current_node.value
+    
+    def len(self):
+        if self.head is None:
+            return 0
+        else:
+            count = 1
+            current_node =self.head
+            while current_node.next:
+                count+=1
+                current_node = current_node.next
+            return count
---
+++ linked_list/main.py
@@ -0,0 +1,12 @@
+from linked_list import Node, LinkedList
+
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    linked_l = LinkedList()
+    # linked_l.append_left(a)
+    # linked_l.append_left(b)
+    print(linked_l.get(0))
+    print(linked_l.get(1))
+    print(linked_l.len())
