09700112439956f48b2e07be8b9d49c406af8267
22ema <yunsung9503@gmail.com>
2023-11-15 23:54:19 +0900

    Update Linked List: implement function
     - append function
     - append left function
     - tail member var
     - insert function
     - remove funtion
    
     #1

--- linked_list/linked_list.py
+++ linked_list/linked_list.py
@@ -6,17 +6,18 @@ class Node():
 class LinkedList():
     def __init__(self):
         self.head = None
-    
+        self.tail = None
     def append_left(self, value):
         node = Node(value)
         if self.head is None:
             self.head = node
+            self.tail = node
         else:
             current_node = self.head
             while(current_node.next):
                 current_node = current_node.next
             current_node.next = node
-
+    ## 시간 복잡도는 N이다.
     def get(self, idx):
         if self.len() <= idx:
             print("out of index")
@@ -26,6 +27,16 @@ class LinkedList():
             current_node = current_node.next
         return current_node.value
     
+    def append(self, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+        else:
+            current_node = self.tail
+            current_node.next = node
+            self.tail = node
+
     def len(self):
         if self.head is None:
             return 0
@@ -36,3 +47,28 @@ class LinkedList():
                 count+=1
                 current_node = current_node.next
             return count
+    
+    def insert(self, idx, value):
+        node = Node(value)
+        if self.len() < idx:
+            print("wrong index number. it's so big")
+        elif self.len() == idx:
+            current_node = self.head
+            for _ in range(idx-1):
+                current_node = current_node.next
+            current_node.next = node
+        else:
+            current_node = self.head
+            for _ in range(idx-1):
+                current_node = current_node.next
+            node.next = current_node.next
+            current_node.next = node
+    
+    def remove(self, idx):
+        if self.len() <= idx:
+            print("wrong index number. it's so bing")
+        else:
+            current_node = self.head
+            for _ in range(idx-1):
+                current_node = current_node.next
+            current_node.next = current_node.next.next
--- linked_list/main.py
+++ linked_list/main.py
@@ -4,9 +4,15 @@ from linked_list import Node, LinkedList
 if __name__ == "__main__":
     a = 1
     b = 2
+    c = 3
+    d = 4
     linked_l = LinkedList()
-    # linked_l.append_left(a)
-    # linked_l.append_left(b)
-    print(linked_l.get(0))
-    print(linked_l.get(1))
-    print(linked_l.len())
+    linked_l.append_left(a)
+    linked_l.append_left(b)
+    linked_l.append_left(c)
+    linked_l.append_left(d)
+    print(f"get: {linked_l.get(0)}, {linked_l.get(1)}, {linked_l.get(2)}, {linked_l.get(3)}")
+    linked_l.insert(4, 5)
+    print(f"get: {linked_l.get(0)}, {linked_l.get(1)}, {linked_l.get(2)}, {linked_l.get(3)}, {linked_l.get(4)}")
+    linked_l.remove(4)
+    print(f"get: {linked_l.get(0)}, {linked_l.get(1)}, {linked_l.get(2)}, {linked_l.get(3)}, {linked_l.get(4)}")
